110547705d2aef79432169b7f7e4e3bfa5bc2b34
Skogen Larson <70291067+SkogenL@users.noreply.github.com>
2024-03-28 11:53:47 -0600

    Added index.html front page, set up project & app to show it.

--- PWA_Demo_App/views.py
+++ PWA_Demo_App/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseBadRequest
+from . import models
+
+def index(request):
+    return render(request, "index.html")
 
 # Create your views here.
